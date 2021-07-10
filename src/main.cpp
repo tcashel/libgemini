414fe0055ee802fc6ea8bbcf4fc58fcb95162bcf
Tripp Cashel <tcashel@gmail.com>
2021-07-10 05:11:13 +0000

    test request to sanbox api

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,11 @@
 
 int main()
 {
-    std::cout << "Hello There People." << std::endl;
+    {
+        std::string btc_ticker_request = "https://api.sandbox.gemini.com/v2/ticker/btcusd";
+        lift::request request{btc_ticker_request};
+        std::cout << "Requesting " << btc_ticker_request << std::endl;
+        const auto& response = request.perform();
+        std::cout << response.data() << std::endl;
+    }
 }
